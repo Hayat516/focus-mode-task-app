df2af327b87145dfd88493a570c3e32d4a3bd00a
hayat boluwatife <adeatoewa@gmail.com>
2026-05-03 10:14:05 +0100

    add delete taske functionality with local storage sync

--- js/task.js
+++ js/task.js
@@ -17,16 +17,54 @@ export function addTask() {
             tasks = [];
         }
 
-        tasks.push(input);
+        const newTask = {
+            text: input,
+            completed: false
+        };
+
+        tasks.push(newTask);
 
         localStorage.setItem("tasks", JSON.stringify(tasks));
 
         const newElement = document.createElement("li");
-        newElement.textContent = input;
+
+        const taskText = document.createElement("span");
+        taskText.textContent = newTask.text;
+
+        const newIcon = document.createElement("span");
+        newIcon.classList.add("fa-solid", "fa-xmark", "delete-icon");
+
+        newIcon.addEventListener("click", deleteTask)
+
+        newElement.appendChild(taskText);
+        newElement.appendChild(newIcon);
+
         taskList.appendChild(newElement);
 
         taskInput.value = ""
     }
+}
+
+export function deleteTask(event) {
+    const icon = event.target
+    const taskItem = icon.parentElement;
+
+    const taskText = taskItem.firstChild.textContent.trim();
+
+    taskItem.remove();
+
+    let tasks = localStorage.getItem("tasks");
+
+    if (tasks) {
+        tasks = JSON.parse(tasks);
+    } else {
+        tasks = [];
+    }
+
+    const updatedTasks = tasks.filter(task => task !== taskText);
+
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+
 }
 
 export function loadTask() {
@@ -35,12 +73,17 @@ export function loadTask() {
     if (!tasks) {
         return;
     }
-    else{
+    else {
         tasks = JSON.parse(tasks);
 
         tasks.forEach(task => {
             const refreshTask = document.createElement("li");
             refreshTask.textContent = task;
+
+            const refreshIcon = document.createElement("span");
+            refreshIcon.classList.add("fa-solid", "fa-xmark", "delete-icon")
+
+            refreshTask.appendChild(refreshIcon)
             taskList.appendChild(refreshTask);
         });
     }
